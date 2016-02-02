29f2366bfdb62b578eda1e7d1886f419365b9f3d
Daniel Jarratt <daniel.jarratt@gmail.com>
2016-02-02 17:11:54 -0600

    ready for testing of multiple resolutions

--- igraph-community-detection.py
+++ igraph-community-detection.py
@@ -13,7 +13,7 @@ from mpl_toolkits.basemap import Basemap
 MINIMUM_EDGE_WEIGHT = 0  # import only edges at this weight or greater
 
 # community detection variables
-RESOLUTION_OF_DETECTION = 0.75
+RESOLUTION_LIST = [0.2, 0.3, 0.4, 0.5, 0.6]
 
 # mapping variables
 MINIMUM_COMMUNITY_SIZE = 3  # draw communities at this size or greater
@@ -25,8 +25,10 @@ nodes = []
 edges = []
 geo_coordinates = {}
 
+# initialize the graph
 G = ig.Graph(directed=True)
 
+# import data
 with open('government/national-gazetteer.tsv') as tsvfile:
     reader = csv.DictReader(tsvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
     for row in reader:
@@ -59,38 +61,44 @@ for node in nodes:
 for edge in edges:
     G.add_edge(edge[0], edge[1], weight=edge[2])
 
-# detect communities
-partition = louvain.find_partition(G, method='RBConfiguration', weight='weight',
-                                   resolution_parameter=RESOLUTION_OF_DETECTION)
-print("Found {0} communities.".format(len(partition)))
+for res in RESOLUTION_LIST:
+    # detect communities
+    partition = louvain.find_partition(G, method='RBConfiguration',
+                                       weight='weight',
+                                       resolution_parameter=res)
+    print("Found {0} communities at resolution {1}.".format(len(partition),
+                                                            res))
 
-# draw the basemap
-plt.figure(figsize=(8, 4))
-m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
-            projection='lcc', lat_1=33, lat_2=45, lon_0=-95, resolution=DRAW)
-if DRAW == 'c':
-    m.shadedrelief()
-    m.drawrivers()
-    DPI = 500
-else:
-    DPI = 100
-m.drawcoastlines()
-m.drawstates()
-m.drawcountries()
+    # draw the basemap
+    plt.figure(figsize=(8, 4))
+    m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
+                projection='lcc', lat_1=33, lat_2=45, lon_0=-95,
+                resolution=DRAW)
+    if DRAW == 'c':
+        m.shadedrelief()
+        m.drawrivers()
+        DPI = 500
+    else:
+        DPI = 100
+    m.drawcoastlines()
+    m.drawstates()
+    m.drawcountries()
 
-# draw points representing counties painted onto the basemap
-colors = iter(cm.rainbow(np.linspace(0, 1, len(partition))))
-for community in partition:
-    color = next(colors)
-    if len(community) >= MINIMUM_COMMUNITY_SIZE:
-        for county in community:
-            x, y = m(G.vs[county]["longitude"], G.vs[county]["latitude"])
-            m.scatter(x, y, 3, marker='o', color=color)
+    # draw points representing counties painted onto the basemap
+    colors = iter(cm.rainbow(np.linspace(0, 1, len(partition))))
+    drawnCommunities = 0
+    for community in partition:
+        color = next(colors)
+        if len(community) >= MINIMUM_COMMUNITY_SIZE:
+            drawnCommunities += 1
+            for county in community:
+                x, y = m(G.vs[county]["longitude"], G.vs[county]["latitude"])
+                m.scatter(x, y, 3, marker='o', color=color)
 
-mapName = MAP_NAME_PREFIX + "-{0}-{1}-{2}-{3}-{4}.png".format(
+    mapName = MAP_NAME_PREFIX + "-{0}-{1}-{2}-{3}-{4}-{5}.png".format(
                                                     datetime.datetime.today(),
                                                     MINIMUM_EDGE_WEIGHT,
                                                     MINIMUM_COMMUNITY_SIZE,
-                                                    RESOLUTION_OF_DETECTION,
-                                                    len(partition))
-plt.savefig(mapName, dpi=DPI, bbox_inches='tight')
+                                                    res, len(partition),
+                                                    drawnCommunities)
+    plt.savefig(mapName, dpi=DPI, bbox_inches='tight')
